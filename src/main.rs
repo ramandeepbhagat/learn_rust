8f0bbe28959994a23cb7a036e86b088394eb5ecb
Ramandeep Bhagat <ramandeepbhagat@protonmail.com>
2022-08-31 19:39:17 +0530

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// Line comments which go to the end of the line.
+/* Block comments which go to the closing delimiter. */
 fn main() {
     println!("Hello, world!");
     print!("I'm a Rustacean!");
